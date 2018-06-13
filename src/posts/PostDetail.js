38d0b27e7791fc7a21917930f9c23b7fc2772f04
CFE <hello@teamcfe.com>
2018-06-13 16:29:04 -0700

    5 - Handling Click Events

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 class PostDetail extends Component {
+  constructor (props) {
+    super(props)
+    this.titleWasClicked = this.titleWasClicked.bind(this)
+  }
+  titleWasClicked (event) {
+    event.preventDefault()
+  }
   render () {
     const {post} = this.props
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.titleWasClicked}>{post.title}</h1>
         <p>{post.content}</p>
       </div>
     )
