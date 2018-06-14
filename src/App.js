bd505212d67b4d73e4848f251d714b40f032a409
CFE <hello@teamcfe.com>
2018-06-14 11:24:57 -0700

    7 - Const Let & Var

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import PostList from './posts/PostList'
+// import PostList from './posts/PostList'
+import ConstLetVar from './learn/ConstLetVar'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <PostList />
+        <ConstLetVar />
       </div>
     )
   }
---
+++ src/learn/ConstLetVar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class ConstLetVar extends Component {
+  constructor (props) {
+    super(props)
+    let someVar = 'hello there'
+    if (someVar == 'hello there') {
+      someVar = 'Hello there again.'
+    }
+
+    var someVar2 = 'method'
+
+    console.log(someVar)
+  }
+  render () {
+    // someVar = 'Hello there again.'
+    console.log(someVar2)
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+export default ConstLetVar
