839b5ba46b19700ae88a554e5641a543003ce770
CFE <hello@teamcfe.com>
2018-06-18 18:13:24 -0700

    15 - Resort Array by Date or Key Value Pair

--- src/posts/PostSorting.js
+++ src/posts/PostSorting.js
@@ -7,11 +7,30 @@ class PostSorting extends Component {
   constructor (props) {
     super(props)
     this.toggleListReverse = this.toggleListReverse.bind(this)
+    this.toggleSortDate = this.toggleSortDate.bind(this)
     this.state = {
-      postList: []
+      postList: [],
+      isOldestFirst: true
     }
   }
 
+  sortByDate () {
+    const {postList} = this.state
+    let newPostList = postList
+    if (this.state.isOldestFirst) {
+      newPostList = postList.sort((a, b) => a.date > b.date)
+    } else {
+      newPostList = postList.sort((a, b) => a.date < b.date)
+    }
+    this.setState({
+      isOldestFirst: !this.state.isOldestFirst,
+      postList: newPostList
+    })
+  }
+
+  toggleSortDate (event) {
+    this.sortByDate()
+  }
   toggleListReverse (event) {
     const {postList} = this.state
     let newPostList = postList.reverse()
@@ -20,8 +39,10 @@ class PostSorting extends Component {
     })
   }
   componentDidMount () {
+    const postList = PostData
     this.setState({
-      postList: PostData
+      isOldestFirst: true,
+      postList: postList
     })
   }
   render () {
@@ -30,6 +51,7 @@ class PostSorting extends Component {
       <div >
         <h1>Hello There</h1>
         <button onClick={this.toggleListReverse}>Reverse Order</button>
+        <button onClick={this.toggleSortDate}>Order by date</button>
         {postList.map((item, index) => {
           return <PostDetail
             post={item}
