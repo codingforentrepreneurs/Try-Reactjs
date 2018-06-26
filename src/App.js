d33a9c7ee0d1faa7864e6fcb503218805c19bd7a
CFE <hello@teamcfe.com>
2018-06-25 17:41:32 -0700

    17 - Component Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import PostSorting from './posts/PostSorting'
+// import PostSorting from './posts/PostSorting'
+import LifeCycle from './learn/LifeCycle'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <PostSorting />
+        <LifeCycle />
       </div>
     )
   }
---
+++ src/learn/LifeCycle.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class LifeCycle extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+  // componentWillMount()
+
+  render () {
+    const {loading} = this.state
+    return <h1>{loading === true ? 'Hello World' : 'False'}</h1>
+  }
+
+  componentDidMount () {
+    // async / api code
+    this.setState({
+      loading: false
+    })
+  }
+}
+
+export default LifeCycle
