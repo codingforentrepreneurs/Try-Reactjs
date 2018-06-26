76b381c3bf74e992067bf1138e1151b896cc068a
CFE <hello@teamcfe.com>
2018-06-25 18:32:57 -0700

    Default App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
-// import PostSorting from './posts/PostSorting'
+import PostSorting from './posts/PostSorting'
 import './App.css'
-
-import WelcomeHereDialog, {AlertBox, SubItem} from './learn/ParentWrapper'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <AlertBox alertType='danger'>I think this is working...</AlertBox>
-        <WelcomeHereDialog />
+        <PostSorting />
       </div>
     )
   }
