cd5c4e250b54b3055a1c917eafedfec01f7961ce
CFE <hello@teamcfe.com>
2018-06-30 15:02:47 -0700

    30 - Using File Input for Image Cropping

--- src/learn/ImgDropAndCrop.js
+++ src/learn/ImgDropAndCrop.js
@@ -16,6 +16,7 @@ class ImgDropAndCrop extends Component {
     constructor(props){
         super(props)
         this.imagePreviewCanvasRef = React.createRef()
+        this.fileInputRef = React.createRef()
         this.state = {
             imgSrc: null,
             imgSrcExt: null,
@@ -120,12 +121,39 @@ class ImgDropAndCrop extends Component {
             }
 
         })
+        this.fileInputRef.current.value = null
+    }
+
+    handleFileSelect = event => {
+        // console.log(event)
+        const files = event.target.files
+        if (files && files.length > 0){
+              const isVerified = this.verifyFile(files)
+             if (isVerified){
+                 // imageBase64Data 
+                 const currentFile = files[0]
+                 const myFileItemReader = new FileReader()
+                 myFileItemReader.addEventListener("load", ()=>{
+                     // console.log(myFileItemReader.result)
+                     const myResult = myFileItemReader.result
+                     this.setState({
+                         imgSrc: myResult,
+                         imgSrcExt: extractImageFileExtensionFromBase64(myResult)
+                     })
+                 }, false)
+
+                 myFileItemReader.readAsDataURL(currentFile)
+
+             }
+        }
     }
   render () {
       const {imgSrc} = this.state
     return (
       <div>
         <h1>Drop and Crop</h1>
+
+        <input ref={this.fileInputRef} type='file' accept={acceptedFileTypes} multiple={false} onChange={this.handleFileSelect} />
         {imgSrc !== null ? 
             <div>
                
